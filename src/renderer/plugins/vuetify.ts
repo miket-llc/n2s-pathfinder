01614834aac119bb9df650f77906be69b196fa2b
miket.io <mdt_@msn.com>
2025-01-08 19:02:43 -0500

    github dark and light themes.

--- src/renderer/plugins/vuetify.ts
+++ src/renderer/plugins/vuetify.ts
@@ -4,8 +4,6 @@ import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.min.css'
 
-import colors from 'vuetify/lib/util/colors.mjs'
-
 export default createVuetify({
   locale: {
     messages: { ko, en, zhHans, zhHant, de, es, ja, fr, ru, pt, nl },
@@ -20,17 +18,34 @@ export default createVuetify({
     }
   },
   theme: {
+    defaultTheme: 'githubDark',
     themes: {
-      light: {
+      githubLight: {
         dark: false,
         colors: {
-          primary: colors.green.darken2
+          background: '#ffffff',
+          surface: '#f6f8fa',
+          primary: '#0969DA',
+          secondary: '#24292f',
+          error: '#cf222e',
+          warning: '#dba617',
+          info: '#58a6ff',
+          success: '#2da44e',
+          text: '#24292f'
         }
       },
-      dark: {
+      githubDark: {
         dark: true,
         colors: {
-          primary: colors.green.darken4
+          background: '#0d1117',
+          surface: '#161b22',
+          primary: '#1f6feb',
+          secondary: '#8b949e',
+          error: '#f85149',
+          warning: '#d29922',
+          info: '#58a6ff',
+          success: '#2ea043',
+          text: '#c9d1d9'
         }
       }
     }
