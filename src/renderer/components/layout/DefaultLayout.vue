76bd4496ec133968d87b2af115c9f3121e0307d5
miket.io <mdt_@msn.com>
2025-01-09 19:45:34 -0500

    left an rirght drawers

--- src/renderer/components/layout/DefaultLayout.vue
+++ src/renderer/components/layout/DefaultLayout.vue
@@ -2,16 +2,37 @@
 import { ref } from 'vue'
 import StatusBar from '@/renderer/components/StatusBar.vue'
 
-const drawerWidth = ref(240)
+const leftDrawerWidth = ref(240)
+const rightDrawerWidth = ref(240)
 
-const startResize = (e: MouseEvent) => {
+const startLeftResize = (e: MouseEvent) => {
   const startX = e.clientX
-  const startWidth = drawerWidth.value
+  const startWidth = leftDrawerWidth.value
 
   const resize = (e: MouseEvent) => {
     const newWidth = startWidth + (e.clientX - startX)
     if (newWidth >= 160 && newWidth <= 600) {
-      drawerWidth.value = newWidth
+      leftDrawerWidth.value = newWidth
+    }
+  }
+
+  const stopResize = () => {
+    window.removeEventListener('mousemove', resize)
+    window.removeEventListener('mouseup', stopResize)
+  }
+
+  window.addEventListener('mousemove', resize)
+  window.addEventListener('mouseup', stopResize)
+}
+
+const startRightResize = (e: MouseEvent) => {
+  const startX = e.clientX
+  const startWidth = rightDrawerWidth.value
+
+  const resize = (e: MouseEvent) => {
+    const newWidth = startWidth - (e.clientX - startX)
+    if (newWidth >= 160 && newWidth <= 600) {
+      rightDrawerWidth.value = newWidth
     }
   }
 
@@ -29,12 +50,20 @@ const startResize = (e: MouseEvent) => {
   <v-app>
     <slot name="titlebar"></slot>
 
-    <div class="navigation-drawer">
-      <div class="resize-handle" @mousedown.prevent="startResize"></div>
+    <div class="left-drawer">
+      <div class="resize-handle" @mousedown.prevent="startLeftResize"></div>
       <v-toolbar density="compact" color="surface" class="drawer-header">
         <v-toolbar-title class="text-caption">Explorer</v-toolbar-title>
       </v-toolbar>
-      <slot name="navigation"></slot>
+      <slot name="left-drawer"></slot>
+    </div>
+
+    <div class="right-drawer">
+      <div class="resize-handle" @mousedown.prevent="startRightResize"></div>
+      <v-toolbar density="compact" color="surface" class="drawer-header">
+        <v-toolbar-title class="text-caption">Details</v-toolbar-title>
+      </v-toolbar>
+      <slot name="right-drawer"></slot>
     </div>
 
     <v-main class="main-content">
@@ -53,19 +82,32 @@ const startResize = (e: MouseEvent) => {
 
 .main-content {
   padding: 0 0 22px 0 !important;
-  margin-left: v-bind(drawerWidth + 'px');
+  margin-left: v-bind(leftDrawerWidth + 'px');
+  margin-right: v-bind(rightDrawerWidth + 'px');
   margin-top: 0 !important;
   height: calc(100vh - 54px) !important;
 }
 
-.navigation-drawer {
+.left-drawer {
   position: absolute;
   top: 32px;
   left: 0;
-  width: v-bind(drawerWidth + 'px');
+  width: v-bind(leftDrawerWidth + 'px');
   height: calc(100vh - 54px);
   background: var(--v-surface-variant);
   border-right: 1px solid var(--v-border-color);
+  display: flex;
+  flex-direction: column;
+}
+
+.right-drawer {
+  position: absolute;
+  top: 32px;
+  right: 0;
+  width: v-bind(rightDrawerWidth + 'px');
+  height: calc(100vh - 54px);
+  background: var(--v-surface-variant);
+  border-left: 1px solid var(--v-border-color);
   display: flex;
   flex-direction: column;
 }
@@ -93,12 +135,19 @@ const startResize = (e: MouseEvent) => {
 
 .resize-handle {
   position: absolute;
-  right: -2px;
   top: 0;
   bottom: 0;
   width: 4px;
   cursor: ew-resize;
   z-index: 2;
+}
+
+.left-drawer .resize-handle {
+  right: -2px;
+}
+
+.right-drawer .resize-handle {
+  left: -2px;
 }
 
 .resize-handle:hover {
